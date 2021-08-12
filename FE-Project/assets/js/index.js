72f6f151bb245c121a16d094a1b3c7240dbb2d1f
bahmanmammadov <bahmanshm@code.edu.az>
2021-08-12 11:02:27 +0400

    template

---
+++ FE-Project/assets/js/index.js
@@ -0,0 +1,12 @@
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val; 
+  }
+  function updateTextInput1(val) {
+    document.getElementById('textInput2').value=val; 
+  }
+  function updateTextInput2(val) {
+    document.getElementById('textInput3').value=val; 
+  }
+  function updateTextInput3(val) {
+    document.getElementById('textInput4').value=val; 
+  }
